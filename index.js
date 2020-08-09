55408586e6dd044cf84be8ce28c69c7b5f98ede8
Evan Byrd <evanbyrd16@gmail.com>
2020-08-08 23:55:40 -0400

    Amwell technical assessment code.

---
+++ index.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+
+async function getData() {
+    try {
+        const resp = await axios.get('https://www.iwillfearnoevil.com/screen/string.txt');
+        return Promise.resolve(resp.data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+(async function main() {
+    const  data = await getData()
+    const dataList = data.split(/\n/)
+    const numericData = dataList.reduce((acc, elem) => {
+        if (Number(elem)) {
+            elem = Number(elem)
+        } else if (elem == "0") {
+            elem = 0
+        } else {
+            return acc
+        }
+
+        if (!acc.includes(elem) && !isNaN(parseFloat(elem)) && isFinite(elem)) {
+            acc.push(elem)
+        }
+        return acc
+    }, [])
+
+    numericData.sort((a, b) => {return a - b})
+    
+    console.log(numericData.slice(0,3))
+})();
